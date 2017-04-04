50ce7ea8d3611f448bfacb1575d42a345b588abf
deevilboy <mlin.products@gmail.com>
2017-04-03 21:57:59 -0700

    fully working v1.0

--- content.js
+++ content.js
@@ -1,33 +1,33 @@
+//Secruity concerns means you cant use variables from other scripts directly in content
+//Thus must pass variables betweem scripts via chrome.runtime.onMessage.addListener()
 
-
-var i =0;
-var x = {}; 
-var trigger;
-
-
+//Listens for Trigger Value to be passed from Popup.js
+//Calls meetupFilter()
 chrome.runtime.onMessage.addListener(
+  var x = {}; 
+  var trigger;    //passed via a message from popup.js;
+
   function(request, sender, sendResponse) {
     console.log(sender.tab ?
                 "from a content script:" + sender.tab.url :
                 "from the extension");
     trigger = request.trigger; 
     meetupFilter();
- 
   });
 
 
+//Parse out low attendance meetup.com events (DOM nodes w/attendees < trigger value) 
+//Trigger value set by user (via popup.html form)
 function meetupFilter() {
-    console.log("beg:");
     
-    $( ".attendee-count" ).each(function() {     
-      x[i] = parseInt((this.attributes.class.ownerElement.innerHTML.slice(1,4).replace(/[\n\r]+/g, '')));
-      i++;
-      console.log("x[i] " + x[i]);
-
-      if (parseInt((this.attributes.class.ownerElement.innerHTML.slice(1,4).replace(/[\n\r]+/g, ''))) < trigger  )
-        {
-        $(this).closest('li.row.event-listing.clearfix.doc-padding').remove()
-        }
-    });
+  //Find # of attendees from tag ID attendee-count and parse out the 3 character and convert to integer
+  //Remove from browser DOM any meetup events w/ Attendees < trigger
+    //Basically remove everything from node <li row event-listing clearfix doc-padding> to <attendee-count>
+  $( ".attendee-count" ).each(function() {     
+    if (parseInt((this.attributes.class.ownerElement.innerHTML.slice(1,4).replace(/[\n\r]+/g, ''))) < trigger  )
+      {
+      $(this).closest('li.row.event-listing.clearfix.doc-padding').remove()
+      }
+  });
 }
 
--- popup.js
+++ popup.js
@@ -1,19 +1,21 @@
+//Inject content.js code into the curremt chrome tab
 function injectTheScript() {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         
         var trigger_value = document.getElementById("trigger_val").value;
         
-        //Send Message to content.js
+        //Send (trigger value) Message to content.js 
         chrome.tabs.sendMessage(tabs[0].id, {trigger: trigger_value}, function(response) {
           console.log(response.farewell);
         });
 
-        // query the active tab, which will be only one tab
+        //Qery the active tab, which will be only one tab
         //and inject the script in it
         chrome.tabs.executeScript(tabs[0].id, {file: "content.js"});
     });
 }
 
+//Listen for button click from popup.html form
 // document.getElementById('clickactivity').addEventListener('click', injectTheScript);
 document.addEventListener("DOMContentLoaded", function(event) {
     var b = document.getElementById('clickactivity').addEventListener('click', injectTheScript, false);
